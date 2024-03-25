b028d2856b21d40cd78e63b9760d9744c22d5ce0
gamvr <gabrielvillanueva11155@gmail.com>
2024-03-24 19:02:39 -0700

    Implemented the basic functionality to run the game. We create a Runnable (game) in the SnakeView.kt which will be returned by a method called startGame(). The snakeActivity will get the Runnable through startGame(). "game" will recursively call itself through a Handler object that will delay execution by 16 ms. In each call whatever the state of the snake is, it will go in that direction. We have buttons in the SnakeActivity that will change the state of direction the snake is going in.

--- app/src/main/java/com/example/minigame/SnakeActivity.kt
+++ app/src/main/java/com/example/minigame/SnakeActivity.kt
@@ -4,21 +4,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 
 import android.widget.Button
-
 import androidx.constraintlayout.widget.ConstraintLayout
+import android.view.View.OnClickListener
+import android.view.View
 
 class SnakeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_snake)
+        // Handler to delay the snake move.
 
-        var snake = SnakeView(this)
+        // Snake head that will move around the screen.
+        var snake: SnakeView = SnakeView(this)
         addContentView(snake, ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT,
             ConstraintLayout.LayoutParams.WRAP_CONTENT))
 
-        val right: Button = findViewById(R.id.snake_move_right)
-        right.setOnClickListener {snake.moveSnakeRight(10)}
+        // Button that will move the snake head across the screen
+        val start: Button = findViewById(R.id.snake_start)
+        val right: Button = findViewById(R.id.snake_right)
+        val left: Button = findViewById(R.id.snake_left)
+        val up: Button = findViewById(R.id.snake_up)
+        val down: Button = findViewById(R.id.snake_down)
 
+        // Listener for start button
+        start.setOnClickListener {snake.startGame().run()}
+
+        // Listener for the dPad buttons.
+        val dPadListener = View.OnClickListener { view ->
+            when (view.id) {
+                R.id.snake_right -> snake.setSnakeRight()
+                R.id.snake_left -> snake.setSnakeLeft()
+                R.id.snake_up -> snake.setSnakeUp()
+                R.id.snake_down -> snake.setSnakeDown()
+            }
+        }
+        // Set listener for buttons
+        right.setOnClickListener(dPadListener)
+        left.setOnClickListener(dPadListener)
+        up.setOnClickListener(dPadListener)
+        down.setOnClickListener(dPadListener)
     }
 
 }
--- app/src/main/java/com/example/minigame/SnakeView.kt
+++ app/src/main/java/com/example/minigame/SnakeView.kt
@@ -5,14 +5,46 @@ import android.graphics.Rect
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Color
-
+import android.os.Handler
+import android.os.Looper
 
 // Custom View that will display the movement and position of the snake.
 class SnakeView(context: Context) : View(context) {
+    // The paint of the snake
     private var paint: Paint = Paint().apply {
-        color = Color.RED
+        color = Color.WHITE
     }
-    private var snake: Rect = Rect(100, 100, 200, 200)
+    // The dimensions of the snake.
+    private var snake: Rect = Rect(50, 50, 100, 100)
+
+    // Stores the direction snake is going in.
+    private var right: Boolean = false
+    private var left: Boolean = false
+    private var up: Boolean = false
+    private var down: Boolean = false
+
+    // Handler to recursively schedule the snake's movement 16 ms apart.
+    private val delay: Handler = Handler(Looper.getMainLooper())
+    // Runnable object that will be recursively called to move the snake
+    private val game: Runnable = object: Runnable {
+        // Method to play the game
+        override fun run() {
+            if (!right && !left && !up && !down) {
+                right =  true
+            }
+            // method to move snake
+            if (right) moveSnakeRight(8)
+            if (left) moveSnakeLeft(8)
+            if (up) moveSnakeUp(8)
+            if (down) moveSnakeDown(8)
+            // delayed recursive call, delay is 16 because 60 fps = 1000ms / 60 "=" 16.67 ms per frame
+            delay.postDelayed(this, 16)
+        }
+    }
+
+    fun startGame(): Runnable {
+        return game
+    }
 
     fun moveSnakeRight(dx: Int) {
         snake.offset(dx, 0)
@@ -29,6 +61,33 @@ class SnakeView(context: Context) : View(context) {
     fun moveSnakeDown(dy: Int) {
         snake.offset(0, dy)
         invalidate()
+    }
+
+    fun setSnakeRight(){
+        right = true
+        left = false
+        up = false
+        down = false
+    }
+    fun setSnakeLeft(){
+        right = false
+        left = true
+        up = false
+        down = false
+    }
+
+    fun setSnakeUp(){
+        right = false
+        left = false
+        up = true
+        down = false
+    }
+
+    fun setSnakeDown(){
+        right = false
+        left = false
+        up = false
+        down = true
     }
 
     override fun onDraw(canvas: Canvas) {
